0dc4e4c970c81bd428cb0397bdffc0b565106581
Somendra singh <93323212+SomenSin@users.noreply.github.com>
2023-04-02 23:59:03 +0530

    Update 0004-median-of-two-sorted-arrays.cpp

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,7 +6,7 @@ public:
         {
             ans.push_back(i);
         }
-        sort(ans.begin(),ans.end());
+        sort(ans.begin(),ans.end()); //SORTING 
         int v=ans.size();
         return v%2==0?(ans[v/2]+ans[v/2-1])/(double)(2):(double)ans[v/2];
         
